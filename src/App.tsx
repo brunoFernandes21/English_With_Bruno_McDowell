8d5e623499cb3923f683c4b1151d331f2adc49c2
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-09-11 22:49:37 +0100

    Add dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import Navigation from "./components/Navigation";
 import { ThemeProvider } from "./components/ThemeProvider";
-import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <ThemeProvider>
       <div className="h-screen">
         <Navigation />
-        <main className="container max-w-7xl mx-auto">
-          <Button>Click me</Button>
-        </main>
+        <main className="container max-w-7xl mx-auto"></main>
       </div>
     </ThemeProvider>
   );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
+import { DarkModeToggle } from "./DarkModeToggle";
 
 const Navigation = () => {
   return (
@@ -7,17 +8,19 @@ const Navigation = () => {
       <nav className="max-w-7xl mx-auto md:px-2 flex flex-col md:flex-row items-center justify-center md:justify-between gap-4 md:gap-0 py-4 md:h-20 lg:h-28">
         <Logo />
         <ul className="flex items-center space-x-6">
-          <li>
-            <Link href="/"> Home</Link>
+          <li className="md:text-md lg:text-xl transition ease-in-out duration-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-pink-500 hover:to-yellow-500">
+            <Link to="/"> Home</Link>
           </li>
-          <li>
-            <Link href="/learning"> Learning</Link>
+          <li className="md:text-md lg:text-xl transition ease-in-out duration-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-pink-500 hover:to-yellow-500">
+            <Link to="/learning"> Learning</Link>
           </li>
-          <li>
-            <Link href="/users"> Users</Link>
+          <li className="md:text-md lg:text-xl transition ease-in-out duration-500 hover:text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-pink-500 hover:to-yellow-500">
+            <Link to="/users"> Users</Link>
           </li>
         </ul>
-        <div>dark</div>
+        <div>
+          <DarkModeToggle />
+        </div>
       </nav>
     </header>
   );
